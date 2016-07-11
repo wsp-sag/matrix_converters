d7245e67c5691e1bf754da275e1950a930aa6635
PeterKucirek <pkucirek@pbworld.com>
2016-07-11 14:40:25 -0400

    Updated `from_binary_matrix()` to be consistent with `emme.from_emx()`

--- matrix_converters/fortran.py
+++ matrix_converters/fortran.py
@@ -4,31 +4,62 @@ import pandas as pd
 from common import coerce_matrix
 
 
-def from_binary_matrix(file):
+def from_binary_matrix(file, zones=None, tall=False):
     """
     Reads a .bin file compatible with Bill Davidson's FORTRAN code
 
     Args:
         file (basestring or File): The file to read.
+        zones (Index or int or None): An Index or Iterable will be interpreted as the zone labels for the matrix rows
+            and columns; returning a DataFrame or Series (depending on `tall`). If an integer is provided, the returned
+            ndarray will be truncated to this 'number of zones'. Otherwise, the returned ndarray will be size to the
+            maximum number of zone dimensioned by the Emmebank.
+        tall (bool):  If True, a 1D data structure will be returned. If `zone_index` is provided, a Series will be
+            returned, otherwise a 1D ndarray.
 
     Returns:
-        Numpy.ndarray of matrix data
+        DataFrame or ndarray
 
     """
     if isinstance(file, basestring):
         with open(file, 'rb') as reader:
             return _from_binary_matrix(reader)
-    return _from_binary_matrix(file)
+    return _from_binary_matrix(file, zones, tall)
 
 
-def _from_binary_matrix(reader):
+def _from_binary_matrix(reader, zones, tall):
     """Lower level function"""
     floats = np.fromfile(reader, dtype=np.float32)
     n_words = len(floats)
     matrix_size = _infer_zones(n_words)
     floats.shape = matrix_size, matrix_size + 1
 
-    return floats[:, 1:]
+    data = floats[:, 1:]
+
+    if zones is None:
+        if tall:
+            n = np.prod(data.shape)
+            data.shape = n
+            return data
+        return data
+
+    if isinstance(zones, (int, np.int_)):
+        data = data[:zones, :zones]
+
+        if tall:
+            data.shape = zones * zones
+            return data
+        return data
+    elif zones is None:
+        return data
+
+    zones = pd.Index(zones)
+    n = len(zones)
+    data = data[:n, :n]
+
+    matrix = pd.DataFrame(data, index=zones, columns=zones)
+
+    return matrix.stack() if tall else matrix
 
 
 def _infer_zones(n_words):
